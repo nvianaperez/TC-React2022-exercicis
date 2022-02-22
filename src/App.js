eb3bb3a17af17d5e48f1a446e41791793272d948
nviana <nvianaperez@gmail.com>
2022-02-22 17:43:07 +0100

    add HashRouter and delete Fragment in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Avatar from "./pages/Avatar";
@@ -13,22 +13,20 @@ import React from "react";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="avatar" element={<Avatar />} />
-            <Route path="calculadora" element={<Calculadora />} />
-            <Route path="comptador" element={<Comptador />} />
-            <Route path="graella-fotos" element={<GraellaFotos />} />
-            <Route path="acordio" element={<Acordio />} />
-            <Route path="toggle-button" element={<ToggleButton />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="avatar" element={<Avatar />} />
+          <Route path="calculadora" element={<Calculadora />} />
+          <Route path="comptador" element={<Comptador />} />
+          <Route path="graella-fotos" element={<GraellaFotos />} />
+          <Route path="acordio" element={<Acordio />} />
+          <Route path="toggle-button" element={<ToggleButton />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
