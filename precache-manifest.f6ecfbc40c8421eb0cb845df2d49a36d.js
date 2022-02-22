50a44abb231d00aa506066071002e880b6733cce
nviana <nvianaperez@gmail.com>
2022-02-22 17:13:01 +0100

    Updates

---
+++ precache-manifest.f6ecfbc40c8421eb0cb845df2d49a36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427064398af6954379ff8523808aad39",
+    "url": "/TC-React2022-exercicis/index.html"
+  },
+  {
+    "revision": "4d83672c7dd13219951a",
+    "url": "/TC-React2022-exercicis/static/css/main.f401c368.chunk.css"
+  },
+  {
+    "revision": "21c340ff2f6a29e8b64a",
+    "url": "/TC-React2022-exercicis/static/js/2.da052bc4.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TC-React2022-exercicis/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "4d83672c7dd13219951a",
+    "url": "/TC-React2022-exercicis/static/js/main.d4b62f85.chunk.js"
+  },
+  {
+    "revision": "74aa507b203bbe746b0e",
+    "url": "/TC-React2022-exercicis/static/js/runtime~main.91fa48d4.js"
+  }
+]);
