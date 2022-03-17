31d7f959ad65b63d04e2824cef413e62e112654f
nviana <nvianaperez@gmail.com>
2022-03-17 20:42:11 +0100

    canvi a BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { NoPage } from "./pages/NoPage";
@@ -9,17 +9,16 @@ import llista from "./pages/llista";
 //UNIC EXPORT DEFAULT
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           {llista.map((el) => (
-          <Route path={el.path} element={el.element} />
+            <Route path={el.path} element={el.element} />
           ))}
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
-
